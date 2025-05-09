72c834c7a5b31e6181fe41375ca773f0394ffa40
sai-snehitha <snehitha2303@gmail.com>
2025-05-09 00:28:16 -0400

    Fix MLflow URI to use Hopsworks tracking

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,5 +1,3 @@
-# scripts/train_model.py
-
 import hopsworks
 import mlflow
 import lightgbm as lgb
@@ -9,6 +7,9 @@ from sklearn.model_selection import train_test_split
 # Step 1: Connect to Hopsworks
 project = hopsworks.login()
 fs = project.get_feature_store()
+
+# Set MLflow URI to Hopsworks
+mlflow.set_tracking_uri(project.get_mlflow_tracking_url())
 
 # Step 2: Load feature data
 fg = fs.get_feature_group(name="citi_bike_features", version=2)
@@ -21,9 +22,6 @@ y = df["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Step 4: Train and log model
-mlflow.set_tracking_uri("https://dagshub.com/sai-snehitha/citi-bike-prediction-system.mlflow")
-mlflow.set_registry_uri("https://dagshub.com/sai-snehitha/citi-bike-prediction-system.mlflow")
-
 with mlflow.start_run():
     model = lgb.LGBMRegressor()
     model.fit(X_train, y_train)
